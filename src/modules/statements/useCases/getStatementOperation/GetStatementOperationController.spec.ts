2f292fe42254ed1d5d1699bf2af7737c4c6abf78
pedroviniciusn <pedrovinicius.dev@gmail.com>
2022-12-19 20:28:11 -0300

    adicionando teste de integracao com banco de dados: obter especifica declarao 'statement'

--- src/modules/statements/useCases/getStatementOperation/GetStatementOperationController.spec.ts
+++ src/modules/statements/useCases/getStatementOperation/GetStatementOperationController.spec.ts
@@ -12,7 +12,7 @@ import createConnection from "../../../../database/index"
 
 let connection: Connection;
 
-describe("Get All Statements", () => {
+describe("Get Statement specific", () => {
   beforeAll(async () => {
     connection = await createConnection();
     await connection.runMigrations();
@@ -32,7 +32,7 @@ describe("Get All Statements", () => {
     await connection.close();
   });
   
-  it("Should be able to get all statements and balance", async () => {
+  it("Should be able to get statement specific", async () => {
     const responseToken = await request(app).post("/api/v1/sessions").send({
       email: "user@test.com",
       password: "test",
@@ -40,23 +40,34 @@ describe("Get All Statements", () => {
 
     const { token } = responseToken.body;
 
-    await request(app).post("/api/v1/statements/deposit").send({
+    const statement = await request(app).post("/api/v1/statements/deposit").send({
       description: "test",
       amount: 100.00,
     }).set({
       Authorization: `Bearer ${token}`
     });
 
-    const response = await request(app).get("/api/v1/statements/balance").set({
+    const response = await request(app).get(`/api/v1/statements/${statement.body.id}`).set({
       Authorization: `Bearer ${token}`
     });
 
-    expect(response.body.statement.length).toBe(1);
-    expect(response.body.balance).toBe(100);
+    expect(response.status).toBe(200);
+
+    expect(response.body).toEqual(
+      expect.objectContaining({
+        id: statement.body.id,
+        user_id: statement.body.user_id,
+        description: statement.body.description,
+        amount: Number(statement.body.amount).toFixed(2),
+        type: statement.body.type,
+        created_at: statement.body.created_at,
+        updated_at: statement.body.updated_at,
+      })
+    );
   });
 
-  it("Should not be able to get all statements and balance if user not authenticated", async () => {
-    const response = await request(app).post("/api/v1/statements/deposit").set({
+  it("Should not be able to get specif statement if user not authenticated", async () => {
+    const response = await request(app).get(`/api/v1/statements/981273987`).set({
       Authorization: `Bearer ${"65b253e6fe67fbc15b0b4d09bdeaabff"}`
     });
 
